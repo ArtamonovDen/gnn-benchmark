07b737068f26023535296a7c894f54ea139891c4
ArtamonovDen <brightcolors46@gmail.com>
2022-04-17 16:18:11 +0300

    Add mutag dataset wraper

--- datasets/igraph_dataset.py
+++ datasets/igraph_dataset.py
@@ -1,11 +1,7 @@
-import csv
 from pathlib import Path
-from statistics import pvariance
 import pandas as pd
-import networkx
 import os
 import torch
-import shutil
 from sklearn import preprocessing
 import igraph as ig
 from torch_geometric.utils.convert import from_networkx
@@ -42,6 +38,10 @@ class IGraphDataset(InMemoryDataset):
         return []
 
     @property
+    def is_undirected():
+        return True
+
+    @property
     def node_attributes(self):
         """
         Should be overridden
@@ -64,18 +64,28 @@ class IGraphDataset(InMemoryDataset):
         # TODO: add a way to override
         labels_df = pd.read_csv(self.label_path, delimiter="\t")
         encoded_labels = self.label_encoder.fit_transform(labels_df["labels"])
-        self.labels = torch.IntTensor(encoded_labels)
+        self.labels = torch.LongTensor(encoded_labels)
 
     def get_edges_from_igraph(self, iG: ig.Graph) -> torch.LongTensor:
         print("Obtaining edge matrix")
         e_num = iG.ecount()
         coo_format_edges = list(zip(*iG.get_edgelist()))
+
+        # add back-edges manually for undirected graph
+        source_nodes, sink_nodes = coo_format_edges[0], coo_format_edges[1]
+        coo_format_edges[0] += sink_nodes
+        coo_format_edges[1] += source_nodes
         pyg_edge_index = torch.LongTensor(coo_format_edges)
         print(f"Edge coo matrix has shape {pyg_edge_index.shape}")
-        assert pyg_edge_index.shape == (2, e_num)
+
+        # TODO: SEEMS WE NEED TO ADD BACK EDGE MANUALLY FOR UNDIRECTED GRAPHS!
+
+        assert pyg_edge_index.shape == (2, e_num * 2)
         return pyg_edge_index
 
-    def get_edge_arrts_from_igraph(self, iG: ig.Graph) -> Optional[torch.FloatTensor]:
+    def get_raw_edge_arrts_from_igraph(
+        self, iG: ig.Graph
+    ) -> Optional[torch.FloatTensor]:
         print("Obtaining edge attibutes...")
         if not self.edge_attributes:
             return None
@@ -91,7 +101,9 @@ class IGraphDataset(InMemoryDataset):
         assert pyg_edge_attrs.shape == (e_num, len(self.edge_attributes))
         return pyg_edge_attrs
 
-    def get_node_arrts_from_igraph(self, iG: ig.Graph) -> Optional[torch.FloatTensor]:
+    def get_raw_node_arrts_from_igraph(
+        self, iG: ig.Graph
+    ) -> Optional[torch.FloatTensor]:
         print("Obtaining node attibutes...")
         if not self.node_attributes:
             return None
@@ -101,13 +113,15 @@ class IGraphDataset(InMemoryDataset):
             one_node_attrs = [node[attr_name] for attr_name in self.node_attributes]
             node_attrs.append(one_node_attrs)
 
+            # TODO: encode one hot <- сначала enc.fit() от всех категорий всех графов
+
         # TODO add feature converter if not float?
         pyg_node_attrs = torch.FloatTensor(node_attrs)
         print(f"Edge attribute matrix has shape {pyg_node_attrs.shape}")
         assert pyg_node_attrs.shape == (n_num, len(self.node_attributes))
         return pyg_node_attrs
 
-    def convert_raw_graphs(self):
+    def convert_raw_graphs(self) -> List[Data]:
         graph_data_list = []
         for i, graph_path in enumerate(
             self.raw_file_names
@@ -116,8 +130,9 @@ class IGraphDataset(InMemoryDataset):
             iG: ig.Graph = ig.load(graph_path)
 
             pyg_edge_index = self.get_edges_from_igraph(iG)
-            pyg_edge_attrs = self.get_edge_arrts_from_igraph(iG)
-            pyg_x = self.get_node_arrts_from_igraph(iG)
+            # TODO: broken for now: self.get_raw_edge_arrts_from_igraph(iG)
+            pyg_edge_attrs = None
+            pyg_x = self.get_raw_node_arrts_from_igraph(iG)
 
             pyg_y = self.labels[i]
 
@@ -128,4 +143,4 @@ class IGraphDataset(InMemoryDataset):
             graph_data_list.append(graph)
 
         data, slices = self.collate(graph_data_list)
-        torch.save((data, slices), self.processed_paths[0])
+        return data, slices
--- datasets/mutag.py
+++ datasets/mutag.py
@@ -1,21 +1,15 @@
-import csv
 from pathlib import Path
-import pandas as pd
-import networkx
 import os
+import numpy as np
+from sklearn.preprocessing import OneHotEncoder
 import torch
-import shutil
-import igraph as ig
-from torch_geometric.utils.convert import from_networkx
-from typing import Dict, Union, List, Optional, Tuple, Callable
 
-from torch_geometric.data import InMemoryDataset
-
-from igraph_dataset import IGraphDataset
+from datasets.igraph_dataset import IGraphDataset
 
 
 class MutagDataset(IGraphDataset):
     def __init__(self, root, label_path, transform=None, pre_transform=None):
+        self.node_encoder = OneHotEncoder()
         super().__init__(root, label_path, transform, pre_transform)
 
     @property
@@ -35,9 +29,36 @@ class MutagDataset(IGraphDataset):
     def process(self):
         # Becasue of if 'process' in self.__class__.__dict__:
         # we need process() method defined in child class
-        self.convert_raw_graphs()
+        data, slices = self.convert_raw_graphs()
+
+        # One-hot encodding node-features
+        x = data.x
+        x = self.node_encoder.fit_transform(x).toarray()
+        x = torch.tensor(x)
+        data.x = x.float()  # TODO: Not sure if it's okay to do that. but seems it works
+        torch.save((data, slices), self.processed_paths[0])
 
 
 root = "/home/friday/projects/hse_gnn/Netpro2vec/data/Mutag"
 label_path = os.path.join(root, "Mutag.txt")
 g = MutagDataset(root, label_path)
+print(g)
+
+
+"""
+from torch_geometric.datasets import TUDataset
+dataset = TUDataset(root="data/TUDataset", name="MUTAG")
+>> dataset.data
+Data(x=[3371, 7], edge_index=[2, 7442], edge_attr=[7442, 4], y=[188])
+>>> g.data
+# Data(x=[3371, 1], edge_index=[2, 3721], edge_attr=[3721, 1], y=[188])
+Data(x=[3371, 7], edge_index=[2, 7442], y=[188]) # UPDATED
+"""
+
+# TODO: WHY?
+"""
+>>> dataset.data.is_undirected()
+False
+>>> g.data.is_undirected()
+True
+"""
